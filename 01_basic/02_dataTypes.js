cb561b273dead1d8bab14140a3875b88531c3bb4
Ashutosh-Mohanty-1131 <ashutoshmohanty339@outlook.com>
2025-04-11 07:39:34 +0000

    DataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";//treat all JS code as newer version
+
+//alert ( 3 + 3 )//we are using nodejs, not browser
+
+console.log(3 
+    + 3)// Code readability should be high
+
+console.log("Ashu")
+
+let name = "Ashu"
+let age = 21
+let isLoggedIn = true
+let state;//(1)
+
+/*
+number = 2 to 53
+bigint = use for big numbers
+strings = always use in single quotes('') or double quotes("")
+boolean = true/false
+null = standalone value
+(1) = undifined
+symbol = unique
+*/
+
+//object
+
+console.log(typeof "Ashu");//it show the type...
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
+
+
+/* mdn */
